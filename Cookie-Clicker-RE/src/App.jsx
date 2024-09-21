8c37787582111fa6eedd5148b5e56e9e359ccc97
Lewis-Allen-2001 <itslewbie01@gmail.com>
2024-09-21 21:49:12 +0100

    third time lucky

--- Cookie-Clicker-RE/src/App.jsx
+++ Cookie-Clicker-RE/src/App.jsx
@@ -38,7 +38,7 @@ export default function App() {
       <h2>Still Better Than What Grandma Used To Make!</h2>
       <h3>CPS = Cookies Per Second!</h3>
       <audio
-        src="public/SuperMario64WaterThemeDireDireDocks.mp3"
+        src="./public/SuperMario64WaterThemeDireDireDocks.mp3"
         muted
         autoPlay
         controls
